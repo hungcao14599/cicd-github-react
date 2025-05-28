356ce4f88a434b121e5494d6c7ea697cdbc05af6
hungnv-phenikaa <hungnv@phenikaa-x.com>
2025-05-28 13:19:17 +0700

    Change App.js test CI/CD

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Barebone react web app - 2022-Jul26 16:53 PST - Add CI/CD using Github Actions
+          Barebone react web app - 2022-Jul26 16:53 PST - Add CI/CD using Github Actions - By Nguyen Viet Hung FE Engineer
         </p>
         <a
           className="App-link"
